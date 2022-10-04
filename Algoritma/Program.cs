db4f9379648e72f707a82673666158ac64aff1ed
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2022-10-04 20:17:04 +0300

    Add project files.

---
+++ Algoritma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteIndex();
+        }
+
+        static void DeleteIndex()
+        {
+            Console.WriteLine("Bir ifade giriniz: ");
+
+            var input = Console.ReadLine();
+
+            var inputSplit = input.Split(separator: ',');
+
+            var result = inputSplit[0].Remove(startIndex: Convert.ToInt16(inputSplit[1]), 1);
+
+            Console.WriteLine(result);
+
+
+
+
+
+        }
+    }
+}
